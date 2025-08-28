2494e24d6630304bdfb3b9b31c54de12a8a42c86
Igor Figueiredo <igorfigueiredors@gmail.com>
2025-08-27 22:51:43 -0300

    player vs ai added

--- src/components/GameContainer/index.jsx
+++ src/components/GameContainer/index.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo, useCallback } from 'react';
 import { GameWrapper, Card, Header, Title, Status, WinCounter, Board, Cell, Mark, Footer, ResetButton } from "./styles";
 
 export function GameContainer() {
-
   const [board, setBoard]= useState(Array(9).fill(null));
-  const [next, setNext] = useState('x');
+  const [turn, setTurn] = useState('x');
   const [winner, setWinner] = useState(null);
   const [winCounter, setWinCounter] = useState({
     'x': 0,
@@ -14,37 +13,101 @@ export function GameContainer() {
 
   const [finished, setFinished] = useState(false);
 
-  const onPlay = (id) => {
-    const nextCondition = next === "x" ? 'o' : 'x'
-    setBoard(( )=> {
-      const newBoard = [...board];
-      
-      newBoard[id] = nextCondition;
-      setNext(nextCondition)
-      return newBoard
-    })
+  const winPatterns = useMemo(() => ([
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+  ]), [])
+
+  const randomAiPlay = useCallback(() => {
+    const emptyCells = board.map((cell, index) => (cell === null ? index : null)).filter((index) => index !== null);
+
+    if (emptyCells.length === 0) return null;
+    return emptyCells[Math.floor(Math.random() * emptyCells.length)];
+  }, [board])
+
+  const aiPlay = useCallback(() => {
+  setBoard(prevValue => {
+    const newBoard = [...prevValue];
+    let index = null;
+
+    for (const [a, b, c] of winPatterns) {
+      if ((newBoard[a] === null && newBoard[b] === 'x' && newBoard[c] === 'x') ||
+          (newBoard[a] === null && newBoard[b] === 'o' && newBoard[c] === 'o')) {
+        index = a; 
+        break;
+      }
+
+      if ((newBoard[a] === 'x' && newBoard[b] === null && newBoard[c] === 'x') ||
+          (newBoard[a] === 'o' && newBoard[b] === null && newBoard[c] === 'o')) {
+        index = b; 
+        break;
+      }
+
+      if ((newBoard[a] === 'x' && newBoard[b] === 'x' && newBoard[c] === null) ||
+          (newBoard[a] === 'o' && newBoard[b] === 'o' && newBoard[c] === null)) {
+        index = c; 
+        break;
+      }
+    }
+
+    // fallback se não encontrou jogada
+    if(index === null) {
+      const random = randomAiPlay();
+      newBoard[random] = 'o';
+    }
+
+    newBoard[index] = 'o'; // IA joga como 'o'
+    return newBoard;
+  });
+
+    setTurn('x');
+  }, [winPatterns, randomAiPlay]);
+
+  // 1 HUMANO
+  const playerPlay = (id) => {
+  // Se a célula já estiver ocupada ou o jogo estiver acabado, não faz nada
+  if (board[id] !== null || finished) return;
   
+  const newBoard = [...board];
+  // 1. Humano joga (O sempre)
+    
+  newBoard[id] = "x";
+
+  setBoard(newBoard);
+  setTurn('o');
+
+};
+
+
+const onPlay = (id) => {
+  if (finished || turn !== "x") return; 
+  playerPlay(id);
+};
+
+useEffect(() => {
+  if (turn === "o" && !finished) {
+    const timer = setTimeout(() => {
+      aiPlay();
+    }, 500); // delayzinho de "pensamento"
+
+    return () => clearTimeout(timer);
   }
+}, [turn, aiPlay, board, finished]);
 
   const onReset = () => {
     setBoard(Array(9).fill(null));
-    setNext('x');
+    setTurn('x');
     setWinner(null);
     setFinished(false);
   }
 
   useEffect(() => {
-    const winPatterns = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
     const winnerFound = winPatterns.find((pattern) => {
       const [a, b, c] = pattern;
       return board[a] && board[a] === board[b] && board[a] === board[c]
@@ -54,11 +117,12 @@ export function GameContainer() {
        setFinished(true);
     }
 
-    const isBoardFull  = board.every(cell => cell !== null);
+    const isBoardFull = board.every(cell => cell !== null);
 
     isBoardFull && setFinished(true);
 
-  }, [board])
+  // eslint-disable-turn-line react-hooks/exhaustive-deps
+  }, [winPatterns, board])
 
 useEffect(() => {
   if(finished) {
@@ -92,7 +156,7 @@ useEffect(() => {
         </Header>
 
         <Board>
-            {board.map(( cell, index ) => {
+            {board.map((cell, index) => {
 
               const variant = cell === 'x' ? 'x' : 'o';
               return (
